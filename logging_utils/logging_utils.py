a9765097e2bb04bab22df67c22c394eb5847b6ea
aadle <aarlmdl@gmail.com>
2026-05-01 12:23:46 +0200

    debugging log folder location

--- logging_utils/logging_utils.py
+++ logging_utils/logging_utils.py
@@ -4,12 +4,15 @@ import logging.config
 import pathlib
 import inspect
 from datetime import datetime
+from pathlib import Path
 
 def get_current_filename():
     caller_file = inspect.stack()[1].filename
     return pathlib.Path(caller_file).stem
 
 def setup_logging(current_filename:str):
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)
     config_file = pathlib.Path(__file__).parent / "config.json"
 
     current_date = datetime.today().strftime("%Y-%m-%d")
